ef9cadb601b3e5b3b8f340c431414ea9a2207037
Peter Howe <pnhowe@gmail.com>
2022-07-28 07:14:14 -0600

    learning rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error;
 use std::fmt;
-use std::iter::Map;
 
+//use std::iter::Map;
 use http::header::{
   HeaderMap, HeaderName, HeaderValue, ACCEPT, ACCEPT_CHARSET, CONTENT_TYPE, USER_AGENT,
 };
@@ -9,12 +9,16 @@ use http::uri::Parts;
 use http::uri::PathAndQuery;
 use http::Method;
 use http::Uri;
+use hyper::body::HttpBody;
 use hyper::client::HttpConnector;
 use hyper::{Body, Client, Request, Response};
 use regex::Regex;
+use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
+//use serde_json::Value;
 use tokio::runtime::Runtime;
+
+const MAX_ALLOWED_RESPONSE_SIZE: u64 = 40960;
 
 #[derive(Serialize, Deserialize)]
 struct Login
@@ -23,52 +27,53 @@ struct Login
   pub password: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct FieldParamater
 {
-  pub name:            String,
-  pub doc:             String,
-  pub path:            String,
+  pub name:     String,
+  pub doc:      String,
+  pub path:     String,
   #[serde(rename = "type")]
-  pub type_:           String,
-  pub length:          u16,
-  pub uri:             String,
-  pub allowed_schemes: Vec<String>,
+  pub type_:    String,
+  pub length:   u16,
+  pub uri:      String,
+  //pub allowed_schemes: Vec<String>,
   //choices        []interface{} `json:"choices"`
-  pub is_array:        bool,
+  pub is_array: bool,
   //default        interface{}   `json:"default"`
-  pub mode:            String,
-  pub required:        bool,
+  pub mode:     String,
+  pub required: bool,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Describe
 {
-  pub name:                String,
-  pub doc:                 String,
-  pub path:                String,
+  pub name:          String,
+  pub doc:           String,
+  pub path:          String,
   // Namespace
-  pub api_version:         String,
-  pub multi_uri_max:       u16,
-  pub namespaces:          Vec<String>,
-  pub models:              Vec<String>,
+  pub api_version:   String,
+  pub multi_uri_max: u16,
+  //pub namespaces:          &'a Vec<String>,
+  //pub models:              &'a Vec<String>,
   // Model
-  pub constants:           Map<String, String>,
-  pub fields:              Vec<FieldParamater>,
-  pub actions:             Vec<String>,
-  pub not_allowed_methods: Vec<String>,
-  pub list_filters:        Map<String, Vec<FieldParamater>>,
+  //pub constants:           &'a Map<String, String>,
+  //pub fields:              &'a Vec<FieldParamater>,
+  //pub actions:             &'a Vec<String>,
+  //pub not_allowed_methods: &'a Vec<String>,
+  //pub list_filters:        &'a Map<String, Vec<FieldParamater>>,
   // Actions
-  pub return_type:         FieldParamater,
+  pub return_type:   FieldParamater,
   #[serde(rename = "static")]
-  pub static_:             bool,
-  pub paramaters:          Vec<FieldParamater>,
+  pub static_:       bool,
+  //pub paramaters:    &'a Vec<FieldParamater>,
 }
 
 #[derive(Debug)]
 pub enum Error
 {
   InvalidURL,
+  ResponseToLarge,
   HTTP(hyper::Error),
   Parse(serde_json::Error),
 }
@@ -79,7 +84,8 @@ impl fmt::Display for Error
   {
     match *self
     {
-      Error::InvalidURL => write!(f, "invalid URL"),
+      Error::InvalidURL => write!(f, "Invalid URL"),
+      Error::ResponseToLarge => write!(f, "Response to Large"),
       Error::HTTP(err) => err.fmt(f),
       Error::Parse(err) => err.fmt(f),
     }
@@ -93,6 +99,7 @@ impl error::Error for Error
     match *self
     {
       Error::InvalidURL => None,
+      Error::ResponseToLarge => None,
       Error::HTTP(ref err) => Some(err),
       Error::Parse(ref err) => Some(err),
     }
@@ -156,7 +163,7 @@ impl CInP
     uri: &'a str,
     data: Option<impl Serialize>,
     headers: Option<HeaderMap>,
-  ) -> Result<Request<Vec<u8>>, Error>
+  ) -> Result<Request<Body>, Error>
   {
     if !self.path_regex.is_match(uri)
     {
@@ -189,10 +196,10 @@ impl CInP
     target_uri.authority = self.api_root.authority.clone();
     let target_uri = Uri::from_parts(target_uri).unwrap();
 
-    let body: Vec<u8> = match data
+    let body: Body = match data
     {
       Some(value) => serde_json::to_vec(&value).unwrap().into(),
-      None => Vec::new(),
+      None => Body::empty(),
     };
 
     println!("------ Request ------");
@@ -212,19 +219,19 @@ impl CInP
   /// ```
   /// pass the type as "Value" to get a Map back
   /// ```
-  async fn request<'a, T>(
+  async fn request<T>(
     &self,
     method: Method,
-    uri: &'a str,
+    uri: &str,
     data: Option<impl Serialize>,
     headers: Option<HeaderMap>,
-  ) -> Result<(T, &HeaderMap), Error>
+  ) -> Result<(T, HeaderMap), Error>
   where
-    T: Deserialize<'a>,
+    T: DeserializeOwned,
   {
     let request = self.create_request(method, uri, data, headers)?;
 
-    let res: Response<Vec<u8>> = match self.client.request(request).await
+    let res: Response<Body> = match self.client.request(request).await
     {
       Ok(res) => res,
       Err(err) => panic!("{:?}", err),
@@ -240,25 +247,38 @@ impl CInP
       // },
     };
 
-    let headers = res.headers();
-    let body = res.into_body();
-
-    let value: T = match serde_json::from_slice(&body)
+    let response_content_length = match res.body().size_hint().upper()
     {
-      Ok(res) => res,
-      Err(e) => return Err(Error::Parse(e)),
+      Some(v) => v,
+      None => MAX_ALLOWED_RESPONSE_SIZE + 1, // Just to protect ourselves from a malicious response
     };
+
+    if response_content_length > MAX_ALLOWED_RESPONSE_SIZE
+    {
+      return Err(Error::ResponseToLarge);
+    }
+
+    let headers = res.headers().clone();
+
+    let bytes = hyper::body::to_bytes(res.into_body()).await?;
+    let value: T = serde_json::from_slice(&bytes).map_err(Error::Parse)?;
 
     Ok((value, headers))
   }
 
-  async fn describe<'a>(&self, uri: &'a str) -> Result<(Describe, &str), Error>
+  async fn describe(&self, uri: &str) -> Result<(Describe, &str), Error>
   {
-    let (data, headers): (Describe, &HeaderMap) = self
-      .request(Method::from_bytes(b"DESCRIBE").unwrap(), uri, None, None)
+    let (data, headers) = self
+      .request::<Describe>(
+        Method::from_bytes(b"DESCRIBE").unwrap(),
+        &uri,
+        None::<()>,
+        None,
+      )
       .await?;
 
-    Ok((data, headers["type"].to_str().unwrap()))
+    let target_type = headers["type"].to_str().unwrap();
+    Ok((data, target_type))
   }
 }
 
@@ -271,17 +291,17 @@ fn main()
   let req = cinp.request(
     Method::from_bytes(b"DESCRIBE").unwrap(),
     "/api/v1/Site/Site",
-    None,
+    None::<()>,
     None,
   );
 
   let (data, req_type) = runtime.block_on(req).expect("got an error");
   println!("{:?}", data);
 
-  let req = cinp.request<Value>(
+  let req = cinp.request(
     Method::from_bytes(b"DESCRIBE").unwrap(),
     "/api/v1/Building/",
-    None,
+    None::<()>,
     None,
   );
 
@@ -293,7 +313,7 @@ fn main()
     password: String::from("root"),
   };
 
-  let req = cinp.request<Value>(
+  let req = cinp.request(
     Method::from_bytes(b"CALL").unwrap(),
     "/api/v1/Auth/User(login)",
     Some(data),
